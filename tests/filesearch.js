e88ae2937e4d9d49f7c931819a10167e1760a0ad
Jony <jcheung@atlassian.com>
2017-05-14 01:01:17 +1000

    test: add tests for filesearch js

---
+++ tests/filesearch.js
@@ -0,0 +1,27 @@
+var assert = require("assert")
+, filesearch = require('../lib/filesearch.js')
+, sh = require('shelljs')
+, cwd = sh.pwd().toString();
+
+
+// TODO - Add meaningful tests duh!
+
+describe('filesearch Module', function(){
+  describe('Should have the following API\'s', function(){
+    
+    describe('findLessImportsInFile()', function(){
+        it('should be a function', function(){
+            assert.equal("function", typeof(filesearch.findLessImportsInFile));
+        });
+        
+        it('should search through a file and find LESS @import statements ', function(done){
+            var file = "./tests/less/test.less";
+            var result = ['lvl1.less', 'lvl2/lvl2.less', 'lvl2/lvl3/lvl3.less'],
+                filesearchresult = filesearch.findLessImportsInFile(file);
+            assert.equal(result.toString(), filesearchresult.toString());
+            done();
+        });
+    })
+
+  })
+})
